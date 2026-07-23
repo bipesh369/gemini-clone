4cea331b94142897125ef13807b24a99e76c032f
bipesh369 <juniorbipesh@gmail.com>
2026-07-23 17:32:32 +0000

    add main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Main.css';
+import { assets } from '../../assets/assets';
 
 const Main = () => {
   return (
-    <div></div>
+    <div className='main'>
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+
+    </div>
   )
 }
 
